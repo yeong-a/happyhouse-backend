04ca517cf5b3b8e4efd6d927dfc77fb8540aa2e6
Yeong-a Jang <jya005@gmail.com>
2021-05-18 15:05:32 +0900

    rename dao files

--- src/main/java/com/ssafy/happyhouse/model/dao/BoardDAO.java
+++ src/main/java/com/ssafy/happyhouse/model/dao/BoardDAO.java
@@ -12,7 +12,10 @@ import com.ssafy.happyhouse.model.dto.Board;
 @Repository
 public interface BoardDAO {
 	int insert(String title, String content) throws SQLException;
+
 	List<Board> select() throws SQLException;
+
 	void delete(String bno) throws SQLException;
+
 	void update(String bno, String title, String content) throws SQLException;
 }
--- src/main/java/com/ssafy/happyhouse/model/dao/HouseDAO.java
+++ src/main/java/com/ssafy/happyhouse/model/dao/HouseDAO.java
@@ -14,9 +14,11 @@ import com.ssafy.happyhouse.model.dto.HouseInfo;
 public interface HouseDAO {
 	// 동별 거래정보 select
 	List<HouseDeal> selectTransaction(String dong) throws SQLException;
+
 	List<HouseDeal> selectTransactionByName(String aptName) throws SQLException;
-	
+
 	// 동별 주소정보 select
 	List<HouseInfo> selectAddress(String dong) throws SQLException;
+
 	List<HouseInfo> selectAddressByName(String aptName) throws SQLException;
 }
